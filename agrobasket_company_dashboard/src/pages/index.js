8022cef48b1db0372ad331e03ba7a88327a8105e
Animesh Ghosh <37587731+ghoshanimesh@users.noreply.github.com>
2021-03-04 12:45:43 +0530

    Update index.js

--- agrobasket_company_dashboard/src/pages/index.js
+++ agrobasket_company_dashboard/src/pages/index.js
@@ -1,10 +1,15 @@
-import React from "react"
+import React, {useEffect} from "react"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <div>
-    <SEO title="Home" />
-  </div>
-)
+const IndexPage = () => {
+  useEffect(() => {
+	   navigate("/loginPage");
+	}, []);
+  return (
+  	<div>
+   	 <SEO title="Home" />
+ 	  </div>
+  )
+}
 
 export default IndexPage
